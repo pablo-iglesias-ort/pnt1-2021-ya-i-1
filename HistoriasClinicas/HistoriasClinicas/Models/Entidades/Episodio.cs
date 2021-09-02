f3c6ab79658ad4bd91e366b6b26583fa766f6fe7
pachuzzz <pachu.peaceandlove@gmail.com>
2021-09-02 16:25:16 -0300

    subida de atributos

--- HistoriasClinicas/HistoriasClinicas/Models/Entidades/Episodio.cs
+++ HistoriasClinicas/HistoriasClinicas/Models/Entidades/Episodio.cs
@@ -1,6 +1,18 @@
-﻿namespace HistoriasClinicas.Models.Entidades
+﻿using System;
+
+namespace HistoriasClinicas.Models.Entidades
 {
     public class Episodio
     {
+        private string Motivo { get; set; }
+        private string Descripcion { get; set; }
+        private DateTime FechaYHoraInicio { get; set; }
+        private DateTime FechaYHoraAlta { get; set; }
+        private DateTime FechaYHoraCierre { get; set; }
+        private bool EstadoAbierto { get; set; }
+        private Evolucion evoluciones { get; set; }
+        private Epicrisis epicrisis { get; set; }
+        private bool EmpleadoRegistra { get; set; } /* en duda si dejar bool */
+
     }
 }
--- HistoriasClinicas/HistoriasClinicas/Models/Entidades/HistoriaClinica.cs
+++ HistoriasClinicas/HistoriasClinicas/Models/Entidades/HistoriaClinica.cs
@@ -4,5 +4,7 @@ namespace HistoriasClinicas.Models.Entidades
 {
     public class HistoriaClinica
     {
+        private Paciente paciente { get; set; }
+        private Episodio Episodios { get; set; }
     }
 }
